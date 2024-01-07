8dc1e8dbb65af476b2f4aaccb640ec03a75a0ed3
河南省WiFi攻防研究所 <90495619+whjwsm@users.noreply.github.com>
2024-01-07 19:14:53 +0800

    Update 学习通实习打卡签到.py
    添加对实习计划打卡的支持

--- 学习通实习打卡签到.py
+++ 学习通实习打卡签到.py
@@ -20,49 +20,78 @@ remark = ""
 session = requests.session()
 res = session.post('https://passport2.chaoxing.com/api/login?name={}&pwd={}&schoolid={}&verify=0'.format(username, password, schoolid), headers=headers).json()
 if res["result"]:
-    res = session.get("https://www.dgsx.chaoxing.com/mobile/clockin/show", headers=headers)
+    res = session.get("http://cqcet.dgsx.chaoxing.com/form/mobile/signIndex", headers=headers)
     txt = res.text
-    if res.status_code == 200:
+    if txt != "您还没有被分配实习计划。":
         if "用户登录状态异常，请重新登录！" not in txt:
-            clockinId = re.search(r'<input id="clockinId" type="hidden" value="(.*)">', txt, re.I).groups()[0]
-            recruitId = re.search(r'<input type="hidden" id="recruitId" value="(.*)" />', txt, re.I).groups()[0]
-            pcid = re.search(r'<input type="hidden" id="pcid" value="(.*)" />', txt, re.I).groups()[0]
-            pcmajorid = re.search(r'<input type="hidden" id="pcmajorid" value="(.*)" />', txt, re.I).groups()[0]
-            address = address
-            geolocation = location
+            planName = re.search(r"planName: '(.*)',", txt, re.I).groups()[0]
+            _type = re.search(r"type: '(.*)',", txt, re.I).groups()[0]
+            signType = re.search(r"signType: '(.*)',", txt, re.I).groups()[0]
+            address = re.search(r'<input type="hidden" id="workAddress" value="(.*)"/>', txt, re.I).groups()[0]
+            geolocation = re.search(r'<input type="hidden" id="workLocation" value="(.*)">', txt, re.I).groups()[0]
             remark = remark
-            should_bntover = re.search(r'''<dd class="should_bntover" selid="(.*)" workStart='(.*)' workEnd='(.*)'>''', txt, re.I).groups()
-            workStart = should_bntover[1]
-            workEnd = should_bntover[2]
             allowOffset = re.search(r'<input type="hidden" id="allowOffset" value="(.*)"/>', txt, re.I).groups()[0]
-            offduty = 0
-            changeLocation = re.search(r'<input type="text" name="location" id="location" value="(.*)" hidden/>', txt, re.I).groups()[0]
-            if re.search(r'<input id="workLocation" type="hidden" >', txt, re.I) is None:
-                offset = re.search(r'<input id="workLocation" type="hidden" value="(.*)">', txt, re.I).groups()[0]
-            else:
-                offset = "NaN"
+            signSettingId = re.search(r'<input type="hidden" id="signSettingId" value="(.*)"/>', txt, re.I).groups()[0]
             data = {
-                "id": clockinId,
-                "type": 0,
-                "recruitId": recruitId,
-                "pcid": pcid,
-                "pcmajorid": pcmajorid,
+                "planName": planName,
+                "type": _type,
+                "signType": signType,
                 "address": address,
                 "geolocation": geolocation,
                 "remark": remark,
-                "workStart": workStart,
-                "workEnd": workEnd,
                 "images": "",
+                "offset": 0,
                 "allowOffset": allowOffset,
-                "offset": offset,
-                "offduty": offduty,
-                "changeLocation": changeLocation
+                "signSettingId": signSettingId
             }
-            res = session.post("https://www.dgsx.chaoxing.com/mobile/clockin/addclockin2", headers=headers, data=data)
+            res = session.post("http://cqcet.dgsx.chaoxing.com/form/mobile/saveSign", headers=headers, data=data)
             print(res.text)
         else:
             print("登录失败，请检查用户名密码后重试")
     else:
-        print("您貌似并没有实习打卡任务")
+        res = session.get("https://www.dgsx.chaoxing.com/mobile/clockin/show", headers=headers)
+        txt = res.text
+        if res.status_code == 200:
+            if "用户登录状态异常，请重新登录！" not in txt:
+                clockinId = re.search(r'<input id="clockinId" type="hidden" value="(.*)">', txt, re.I).groups()[0]
+                recruitId = re.search(r'<input type="hidden" id="recruitId" value="(.*)" />', txt, re.I).groups()[0]
+                pcid = re.search(r'<input type="hidden" id="pcid" value="(.*)" />', txt, re.I).groups()[0]
+                pcmajorid = re.search(r'<input type="hidden" id="pcmajorid" value="(.*)" />', txt, re.I).groups()[0]
+                address = address
+                geolocation = location
+                remark = remark
+                should_bntover = re.search(r'''<dd class="should_bntover" selid="(.*)" workStart='(.*)' workEnd='(.*)'>''', txt, re.I).groups()
+                workStart = should_bntover[1]
+                workEnd = should_bntover[2]
+                allowOffset = re.search(r'<input type="hidden" id="allowOffset" value="(.*)"/>', txt, re.I).groups()[0]
+                offduty = 0
+                changeLocation = re.search(r'<input type="text" name="location" id="location" value="(.*)" hidden/>', txt, re.I).groups()[0]
+                if re.search(r'<input id="workLocation" type="hidden" >', txt, re.I) is None:
+                    offset = re.search(r'<input id="workLocation" type="hidden" value="(.*)">', txt, re.I).groups()[0]
+                else:
+                    offset = "NaN"
+                data = {
+                    "id": clockinId,
+                    "type": 0,
+                    "recruitId": recruitId,
+                    "pcid": pcid,
+                    "pcmajorid": pcmajorid,
+                    "address": address,
+                    "geolocation": geolocation,
+                    "remark": remark,
+                    "workStart": workStart,
+                    "workEnd": workEnd,
+                    "images": "",
+                    "allowOffset": allowOffset,
+                    "offset": offset,
+                    "offduty": offduty,
+                    "changeLocation": changeLocation
+                }
+                res = session.post("https://www.dgsx.chaoxing.com/mobile/clockin/addclockin2", headers=headers, data=data)
+                print(res.text)
+            else:
+                print("登录失败，请检查用户名密码后重试")
+        else:
+            print("您貌似并没有实习打卡任务")
 else:
     print("登录失败，请检查您的用户名密码是否正确")
